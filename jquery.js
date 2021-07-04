b9c7d74e2e5df4ff9dfab25ba72f34f3fe1a4c9f
Olga <53094440+OlgaOmmm@users.noreply.github.com>
2021-07-04 15:14:00 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,20 +1,19 @@
-//to calc random summer temperature
+//to calc random temperature for water
 function randomTemperature(){
-	var calcTemperature = Math.floor(Math.random() * 12) + 20;
+	var calcTemperature = Math.floor(Math.random() * 8) + 15;
 	return ('+' + calcTemperature);
 }
 
 $(document).ready(function(){
-	//to set random temperature to h3 from WeatherForecastSub ("NEXT MONTH forecast:")
-	$('#WeatherForecastSub h3').text(randomTemperature());
-	$('#WeatherForecastSub h3').css({'font-size':'2rem'});
-	$('#WeatherForecastSub h3').css({'color':'#000'});
+	//to set random temperature to h3 from WaterSub ("Water:")
+	$('#WaterSub h3').text(randomTemperature());
+	$('#WaterSub h3').css({'font-size':'2.5rem'});
+	$('#WaterSub h3').css({'color':'#000'});
 	
-	//to get current temperature (first symbols from array) from h3 
-	var GetTemperature = $('h3').text();
-	var GetTemperatureNow = GetTemperature.substr(0,3);
-	//to set current temperature to h3 from TemperatureNowSub ("Temperature NOW:")	
+	//to get current temperature (first 3 symbols from array) from h3 
+	var GetTemperatureNow = ($('h3').text()).substr(0,3);
+	//to set current temperature to h3 from TemperatureNowSub ("NOW:")	
 	$('#TemperatureNowSub h3').text(GetTemperatureNow);
-	$('#TemperatureNowSub h3').css({'font-size':'3.5rem'});
+	$('#TemperatureNowSub h3').css({'font-size':'2.5rem'});
 	$('#TemperatureNowSub h3').css({'color':'#fff000'});
 });
